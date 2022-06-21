bcf0017204ec877ef003698f4d6f64681f0b1971
Christopher <chrystopam@gmail.com>
2022-06-21 07:57:41 +0100

    Testers changed

--- validation/org/testers.js
+++ validation/org/testers.js
@@ -1,43 +1,73 @@
-const Validator = require("validator");
-const isEmpty = require("../isEmpty");
+const Validator = require('validator')
+const isEmpty = require('../isEmpty')
 
 module.exports = function validateTestersInput(data) {
-  let errors = {};
+  let errors = {}
 
-  data.fullName = isEmpty(data.fullName) ? "" : data.fullName;
-  data.department = isEmpty(data.department) ? "" : data.department;
-  data.email = isEmpty(data.email) ? "" : data.email;
+  data.email = isEmpty(data.email) ? '' : data.email
+  data.firstname = isEmpty(data.firstname) ? '' : data.firstname
+  data.surname = isEmpty(data.surname) ? '' : data.surname
+  data.middlename = isEmpty(data.middlename) ? '' : data.middlename
+  data.username = isEmpty(data.username) ? '' : data.username
+  data.department = isEmpty(data.department) ? '' : data.department
 
-  // Fullname validations
-  if ((!Validator.isLength(data.fullName, { min: 2, max: 30 }))) {
-    errors.fullName = "Name must be between 2 and 30 characters";
+  // Firstname validations
+  if (!Validator.isLength(data.firstname, { min: 2, max: 30 })) {
+    errors.firstname = 'Name must be between 2 and 30 characters'
   }
 
-  if (Validator.isEmpty(data.fullName)) {
-    errors.fullName = "Fullname field is required";
+  if (Validator.isEmpty(data.firstname)) {
+    errors.firstname = 'First Name field is required'
+  }
+
+  // Surname validations
+  if (!Validator.isLength(data.surname, { min: 2, max: 30 })) {
+    errors.surname = 'Name must be between 2 and 30 characters'
+  }
+
+  if (Validator.isEmpty(data.surname)) {
+    errors.surname = 'Surname field is required'
+  }
+
+  // Middlename validations
+  if (!Validator.isLength(data.middlename, { min: 2, max: 30 })) {
+    errors.middlename = 'Name must be between 2 and 30 characters'
+  }
+
+  if (Validator.isEmpty(data.middlename)) {
+    errors.middlename = 'Middle Name field is required'
+  }
+
+  // Username validations
+  if (!Validator.isLength(data.username, { min: 2, max: 30 })) {
+    errors.username = 'Name must be between 2 and 30 characters'
+  }
+
+  if (Validator.isEmpty(data.username)) {
+    errors.username = 'Username field is required'
   }
 
   // Deepartment validations
-  if ((!Validator.isLength(data.department, { min: 2, max: 30 }))) {
-    errors.department = "Department must be between 2 and 30 characters";
+  if (!Validator.isLength(data.department, { min: 2, max: 30 })) {
+    errors.department = 'Department must be between 2 and 30 characters'
   }
 
   if (Validator.isEmpty(data.department)) {
-    errors.department = "Department field is required";
+    errors.department = 'Department field is required'
   }
 
   // Email validations
 
   if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid";
+    errors.email = 'Email is invalid'
   }
 
   if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
+    errors.email = 'Email field is required'
   }
 
   return {
     errors,
     isValid: isEmpty(errors),
-  };
-};
+  }
+}

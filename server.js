2772e0d976edff08efbaccaa22c9a57fb7ec7f08
Chioma <anigboguchioma31@gmail.com>
2022-06-23 11:52:09 +0100

    commiting changes to validtion

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.use('/api/profile', profile);
 app.use('/api/complaint', complaint);
 app.use('/api/quiz', quizRouter);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running  on port ${port}`));
--- validation/org/testers.js
+++ validation/org/testers.js
@@ -5,46 +5,16 @@ module.exports = function validateTestersInput(data) {
   let errors = {}
 
   data.email = isEmpty(data.email) ? '' : data.email
-  data.firstname = isEmpty(data.firstname) ? '' : data.firstname
-  data.surname = isEmpty(data.surname) ? '' : data.surname
-  data.middlename = isEmpty(data.middlename) ? '' : data.middlename
-  data.username = isEmpty(data.username) ? '' : data.username
+  data.firstname = isEmpty(data.firstName) ? '' : data.firstName
   data.department = isEmpty(data.department) ? '' : data.department
 
   // Firstname validations
-  if (!Validator.isLength(data.firstname, { min: 2, max: 30 })) {
-    errors.firstname = 'Name must be between 2 and 30 characters'
+  if (!Validator.isLength(data.firstName, { min: 2, max: 30 })) {
+    errors.firstName = 'Name must be between 2 and 30 characters'
   }
 
-  if (Validator.isEmpty(data.firstname)) {
-    errors.firstname = 'First Name field is required'
-  }
-
-  // Surname validations
-  if (!Validator.isLength(data.surname, { min: 2, max: 30 })) {
-    errors.surname = 'Name must be between 2 and 30 characters'
-  }
-
-  if (Validator.isEmpty(data.surname)) {
-    errors.surname = 'Surname field is required'
-  }
-
-  // Middlename validations
-  if (!Validator.isLength(data.middlename, { min: 2, max: 30 })) {
-    errors.middlename = 'Name must be between 2 and 30 characters'
-  }
-
-  if (Validator.isEmpty(data.middlename)) {
-    errors.middlename = 'Middle Name field is required'
-  }
-
-  // Username validations
-  if (!Validator.isLength(data.username, { min: 2, max: 30 })) {
-    errors.username = 'Name must be between 2 and 30 characters'
-  }
-
-  if (Validator.isEmpty(data.username)) {
-    errors.username = 'Username field is required'
+  if (Validator.isEmpty(data.firstName)) {
+    errors.firstName = 'First Name field is required'
   }
 
   // Deepartment validations

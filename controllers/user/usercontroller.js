2714a7053e2c63c0e7fad13860c7b0498baf8422
Chioma <anigboguchioma31@gmail.com>
2022-06-28 14:37:15 +0100

    committed

--- controllers/user/usercontroller.js
+++ controllers/user/usercontroller.js
@@ -19,39 +19,39 @@ module.exports = {
       return res.status(400).json(errors);
     }
 
-    User.findAll({ email: req.body.email })
-    .then((user) => {
-      if (user) {
-        return res.status(400).json({ email: "Email already exists" });
-      } else {
-        const avatar = gravatar.url(req.body.email, {
-          s: "200",
-          r: "pg",
-          d: "mm",
-        });
+    // User.findOne({ email: req.body.email })
+    // .then((user) => {
+    //   if (user) {
+    //     return res.status(400).json({ email: "Email already exists" });
+    //   } else {
+    //     const avatar = gravatar.url(req.body.email, {
+    //       s: "200",
+    //       r: "pg",
+    //       d: "mm",
+    //     });
 
-        const newUser = new User({
-          firstname: req.body.firstname,
-          surname: req.body.surname,
-          username: req.body.username,
-          email: req.body.email,
-          password: req.body.password,
-          password2: req.body.password2,
-          avatar,
-        });
+    //     const newUser = new User({
+    //       firstname: req.body.firstname,
+    //       surname: req.body.surname,
+    //       username: req.body.username,
+    //       email: req.body.email,
+    //       password: req.body.password,
+    //       password2: req.body.password2,
+    //       avatar,
+    //     });
 
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then((user) => res.json(user))
-              .catch((err) => console.log(err));
-          });
-        });
-      }
-    });
+    //     bcrypt.genSalt(10, (err, salt) => {
+    //       bcrypt.hash(newUser.password, salt, (err, hash) => {
+    //         if (err) throw err;
+    //         newUser.password = hash;
+    //         newUser
+    //           .save()
+    //           .then((user) => res.json(user))
+    //           .catch((err) => console.log(err));
+    //       });
+    //     });
+    //   }
+    // });
   },
 
   loginUser: (req, res) => {

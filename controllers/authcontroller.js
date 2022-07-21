f8ecde8f2077e77717496fd469212d3539668ba6
Chioma <anigboguchioma31@gmail.com>
2022-07-21 14:26:05 +0100

    frontend apis

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -7,7 +7,7 @@ module.exports = {
     return res
       .status(403)
       .json({
-        msg: "You need a minimum level of ORG auth to access this route.",
+        // msg: "You need a minimum level of ORG auth to access this route.",
       });
   },
 };
